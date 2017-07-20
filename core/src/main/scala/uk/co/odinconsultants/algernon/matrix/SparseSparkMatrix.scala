0a85bebcd4fcdc13a0e605edc4011a99d639936b
PhillHenry <phillhenry@gmail.com>
2017-07-20 08:18:04 +0100

    Minor refactoring.

--- core/src/main/scala/uk/co/odinconsultants/algernon/matrix/SparseSparkMatrix.scala
+++ core/src/main/scala/uk/co/odinconsultants/algernon/matrix/SparseSparkMatrix.scala
@@ -2,22 +2,25 @@ package uk.co.odinconsultants.algernon.matrix
 
 import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
 import scala.reflect.runtime.universe.TypeTag
+import Numeric.Implicits._
 
-case class MatrixCell[T](i: Long, j: Long, x: T) // TODO make x configurable
+case class MatrixCell[T](i: Long, j: Long, x: T)
 
 object SparseSparkMatrix {
 
-  type SparseSpark[T]   = Dataset[MatrixCell[T]]
+  type SparseSpark[T] = Dataset[MatrixCell[T]]
+  type CellPairOp[T]  = ((MatrixCell[T], MatrixCell[T])) => MatrixCell[T]
+  type CellOp[T]      = (MatrixCell[T], MatrixCell[T]) => MatrixCell[T]
 
   implicit class SparseSparkOps[T: Encoder: TypeTag: Numeric](ds: SparseSpark[T]) {
     def multiply(other: SparseSpark[T])(implicit session: SparkSession): SparseSpark[T] = {
       import session.implicits._
 
-      val mathOps = implicitly[Numeric[T]]
+      val mathOps                   = implicitly[Numeric[T]]
 
-      val multiplied: ((MatrixCell[T], MatrixCell[T])) => MatrixCell[T] = { case (x, y) => MatrixCell(x.i, y.j, mathOps.times(x.x, y.x) ) }
+      val multiplied: CellPairOp[T] = { case (x, y) => MatrixCell(x.i, y.j, mathOps.times(x.x, y.x) ) }
 
-      val added: (MatrixCell[T], MatrixCell[T]) => MatrixCell[T] = { case (x, y) => x.copy(x = mathOps.plus(x.x, y.x)) }
+      val added:      CellOp[T]     = { case (x, y) => x.copy(x = mathOps.plus(x.x, y.x)) }
 
       ds.joinWith(other, ds("j") === other("i"), "inner").map(multiplied).groupByKey(c => (c.i, c.j)).reduceGroups(added).map(_._2)
     }
